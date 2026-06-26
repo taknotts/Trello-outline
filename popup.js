d4a36f98436795e9b8d0f6f17b9225120660e6d0
Trina Knotts <trina@trinas-air.ucdmc.ucdavis.edu>
2026-06-26 15:34:42 -0700

    Add editable outline prototype

--- popup.js
+++ popup.js
@@ -1,1 +1,28 @@
-console.log("Popup loaded");
+const outline = document.getElementById("outline");
+const addButton = document.getElementById("addItem");
+
+function addItem(text = "") {
+
+    const input = document.createElement("input");
+
+    input.type = "text";
+    input.placeholder = "New item";
+    input.value = text;
+
+    outline.appendChild(input);
+
+    input.focus();
+
+    input.addEventListener("keydown", function(e){
+
+        if(e.key === "Enter"){
+            addItem();
+        }
+
+    });
+
+}
+
+addButton.addEventListener("click", function(){
+    addItem();
+});
