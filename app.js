077634a70baa031bb6bddaee4b33f72a2b663011
Trina Knotts <trina@trinas-air.ucdmc.ucdavis.edu>
2026-06-26 15:12:18 -0700

    Add Project Outline card button

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ window.TrelloPowerUp.initialize({
   "card-buttons": function (t, options) {
     return [
       {
+        icon: {
+          dark: "./images/icon.svg",
+          light: "./images/icon.svg"
+        },
         text: "Project Outline",
         callback: function (t) {
           return t.popup({
             title: "Project Outline",
-            url: "./index.html",
-            height: 300
+            url: "./popup.html",
+            height: 200
           });
         }
       }
